e8bd1e5509d1d16ce6f1dca6bb297ed845fc524d
ericmelton1 <ericmelton1@e7bc6108-1c3b-11df-90e4-2d055072c7ef>
2010-02-19 21:06:33 +0000

    Improved logout process

--- processes/logout.php
+++ processes/logout.php
@@ -1,7 +1,17 @@
 <?php
-//reset the php session
 session_start();
-session_regenerate_id();
+
+// kill the session
+$_SESSION = array();
+
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000,
+        $params["path"], $params["domain"],
+        $params["secure"], $params["httponly"]
+    );
+}
+
 session_destroy();
 
 //redirect
