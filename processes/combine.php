31d3de1345619f6ecf719d895e4edcfbbeafb7ba
Ulrich Hahn <uhahn@hsu-bibliothek.de>
2026-02-03 15:44:44 +0100

    replace each()  - deprecated in PHP 8
    (cherry picked from commit 207d72fa7c7767f3a5b6da280ce246abc6a07e88)

--- processes/combine.php
+++ processes/combine.php
@@ -48,7 +48,7 @@
 	
 	// Determine last modification date of the files
 	$lastmodified = 0;
-	while (list(,$element) = each($elements)) {
+	foreach($elements as $element) {
 		$path = realpath($base . '/' . $element);
 	
 		if (($type == 'javascript' && substr($path, -3) != '.js') || 
@@ -124,7 +124,7 @@
 		// Get contents of the files
 		$contents = '';
 		reset($elements);
-		while (list(,$element) = each($elements)) {
+		foreach($elements as $element) {
 			$path = realpath($base . '/' . $element);
 			$contents .= "\n\n" . file_get_contents($path);
 		}
